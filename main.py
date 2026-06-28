36b8d52c0c45e345a700ad214cacfbbc1f78262a
Zaid <mkhalidkachhawa@gmail.com>
2026-06-28 07:17:28 +0530

    Some changes in the code have been done

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score,mean_absolute_error
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 
 df = pd.read_csv('D:/Downloads/House_Prices.csv')
@@ -31,11 +33,24 @@ print("\nPredicted Price : ")
 print(y_pred)
 
 mse = mean_squared_error(y_test,y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+rmse = np.sqrt(mse)
 r2 = r2_score(y_test,y_pred)
+
 print("\nModel Evaluation : ")
-print("\nMean Squared Error : ",mse)
+print("\nMean Squared Error (MSE) : ",mse)
+print("Mean Absolute Error (MAE):", mae)
+print("Root Mean Squared Error (RMSE):", rmse)
 print("\nR2 Score : ",r2)
 
+plt.figure(figsize=(8,6))
+plt.scatter(y_test, y_pred)
+plt.plot([y.min(), y.max()], [y.min(), y.max()], 'r--')
+plt.xlabel("Actual House Price")
+plt.ylabel("Predicted House Price")
+plt.title("Actual vs Predicted House Price")
+plt.grid(True)
+plt.show()
 # Step 9: Take input from the user for Linear Regression
 print("\n--- Taking the input for prediction ---")
 SqFt_lr = float(input("\nEnter the House Size in terms of SqFt: "))
@@ -63,3 +78,14 @@ predicted_price_lr = model.predict(new_data_lr)
 print("\n===== House Details  =====")
 print(new_data_lr)
 print(f"\nPredicted House Price (Linear Regression): {predicted_price_lr[0]:.2f}")
+print("\n========== Model Parameters ==========")
+
+print("Coefficient (SqFt)                :", model.coef_[0])
+print("Coefficient (Bedrooms)            :", model.coef_[1])
+print("Coefficient (Bathrooms)           :", model.coef_[2])
+print("Coefficient (Offers)              :", model.coef_[3])
+print("Coefficient (Brick_Yes)           :", model.coef_[4])
+print("Coefficient (Neighborhood_North)  :", model.coef_[5])
+print("Coefficient (Neighborhood_West)   :", model.coef_[6])
+
+print("\nIntercept :", model.intercept_)
